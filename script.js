a2934a1d7c00c7381eb23d992aa089f202d01685
LouisCorin <iordanescucorinlouis@gmail.com>
2022-08-15 11:17:56 +0300

    Add rock,paper,scissors buttons with emojy img embeded on them

--- script.js
+++
@@ -1,8 +0,0 @@
-let hands = ["rock", "paper", "scissor"]
-
-function myHand(){
-    let randomNr = Math.floor(Math.random() * hands.length)
-    return hands[randomNr]
-    
-}
-console.log(myHand())
